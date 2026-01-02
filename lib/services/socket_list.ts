16068bfe10b7fc91bc4b2f009a59cf3251ab0317
austin cheney <info@prettydiff.com>
2026-01-02 16:11:23 -0600

    screenshots

--- lib/services/socket_list.ts
+++
@@ -1,8 +0,0 @@
-
-import socket_list_build from "../transmit/socket_list_build.ts";
-
-const socket_list = function services_socketList():void {
-    socket_list_build();
-};
-
-export default socket_list;
