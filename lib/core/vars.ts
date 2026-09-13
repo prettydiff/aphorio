362d969c6f97e25cb6c0322d21930b46b0d7e08f
cheney <cheney@am-10f005f36d49>
2026-09-13 15:16:55 -0500

    continuing to test ufw firewall rules

--- lib/core/vars.ts
+++ lib/core/vars.ts
@@ -34,9 +34,9 @@ const vars:core_vars = {
                     // default debian firewall is ufw
                     firewall_allow_out: "sudo ufw allow out #/tcp",
                     // default debian firewall is ufw
-                    firewall_deny_in: "sudo ufw delete allow #/tcp",
-                    // ufw does not make use of this command
-                    firewall_deny_out: "",
+                    firewall_deny_in: "sudo ufw delete allow in #/tcp",
+                    // default debian firewall is ufw
+                    firewall_deny_out: "sudo ufw delete allow out #/tcp",
                     // open an application
                     open: "xdg-open",
                     // gets disk partition data, unused in linux as the disk command is enough
